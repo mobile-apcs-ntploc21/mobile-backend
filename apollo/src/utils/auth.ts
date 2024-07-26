e436d485b9c2345ee7c106ecd092ad827c1736cc
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-07-26 21:24:55 +0700

    BE-53: Fix server crashing if API provided invalid token.

--- apollo/src/utils/auth.ts
+++ apollo/src/utils/auth.ts
@@ -1,14 +1,14 @@
-import jwt from 'jsonwebtoken';
-import UserModel from '../models/user';
-import { config } from '../config';
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user";
+import { config } from "../config";
 
 export const getUserIdByToken = async (token: string): Promise<string> => {
-  if (!token) throw new Error('Missing auth token');
+  if (!token) throw new Error("Missing auth token");
 
   const decoded = jwt.verify(token, config.JWT_SECRET) as jwt.JwtPayload;
   const user = await UserModel.findById(decoded.id);
   if (!user)
-    throw new Error('The user belonging to this token does no longer exist');
+    throw new Error("The user belonging to this token does no longer exist");
 
   return user.id;
 };
