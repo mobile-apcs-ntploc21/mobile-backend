b372e540573631bdf671cffca9845991d050068c
ntploc21_zalora <loc.nguyenthanh@zalora.com>
2024-08-05 17:44:31 +0700

    BE-65: Change Friend Relationship _id to store ObjectId instead of String

--- apollo/src/models/relationship.ts
+++ apollo/src/models/relationship.ts
@@ -1,14 +1,18 @@
 import mongoose, { model, Schema } from "mongoose";
 import validator from "validator";
+import ModelNames from "./modelNames";
+
+export enum RelationshipType {
+  PENDING_FIRST_SECOND = "PENDING_FIRST_SECOND",
+  PENDING_SECOND_FIRST = "PENDING_SECOND_FIRST",
+  FRIEND = "FRIEND",
+  BLOCK_FIRST_SECOND = "BLOCK_FIRST_SECOND",
+  BLOCK_SECOND_FIRST = "BLOCK_SECOND_FIRST",
+}
 
 interface IRelationship {
-  _id: { user_first_id: string; user_second_id: string };
-  type:
-    | "PENDING_FIRST_SECOND"
-    | "PENDING_SECOND_FIRST"
-    | "FRIEND"
-    | "BLOCK_FIRST_SECOND"
-    | "BLOCK_SECOND_FIRST";
+  _id: { user_first_id: Schema.Types.ObjectId; user_second_id: Schema.Types.ObjectId };
+  type: RelationshipType;
   last_modified: Date;
 }
 
@@ -16,23 +20,19 @@ const relationshipSchema = new Schema<IRelationship>(
   {
     _id: {
       user_first_id: {
-        type: String,
-        required: true,
+        type: Schema.Types.ObjectId,
+        ref: ModelNames.User,
+        required: [true, "User ID for user 1 is required!"],
       },
       user_second_id: {
-        type: String,
-        required: true,
+        type: Schema.Types.ObjectId,
+        ref: ModelNames.User,
+        required: [true, "User ID for user 2 is required!"],
       },
     },
     type: {
       type: String,
-      enum: [
-        "PENDING_FIRST_SECOND",
-        "PENDING_SECOND_FIRST",
-        "FRIEND",
-        "BLOCK_FIRST_SECOND",
-        "BLOCK_SECOND_FIRST",
-      ],
+      enum: Object.values(RelationshipType),
       required: true,
     },
     last_modified: {
@@ -44,7 +44,7 @@ const relationshipSchema = new Schema<IRelationship>(
 );
 
 const RelationshipModel = mongoose.model<IRelationship>(
-  "Relationship",
+  ModelNames.Relationship,
   relationshipSchema
 );
 export default RelationshipModel;
