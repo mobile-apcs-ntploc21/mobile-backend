79014ac2ea530890ff94d5f20e69eda9644b6380
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-06 21:16:36 +0700

    BE-68: Add channel related into typeDefs index

--- apollo/src/graphql/typedefs/index.ts
+++ apollo/src/graphql/typedefs/index.ts
@@ -1,18 +1,25 @@
 import { gql } from "apollo-server-express";
 import { mergeTypeDefs } from "@graphql-tools/merge";
 
+/// User
 import userSchema from "./user";
 import relationshipSchema from "./relationship";
 import { userStatusSchema_API, userStatusSchema_Ws } from "./user_status";
-import serverSchema from "./server";
-import serverMemberSchema from "./server_member";
 import userSettingsSchema from "./userSettings";
 import {
   apolloTypedefs as userProfileApollo,
   wsTypedefs as userProfileWs,
 } from "./user_profile";
+
+/// Server
+import serverSchema from "./server";
+import serverMemberSchema from "./server_member";
 import serverEmoji from "./serverEmoji";
+
+/// Channel
 import channel from "./Channel/channel";
+import channel_permission from "./Channel/channel_permission";
+import category from "./Channel/category";
 
 const linkedSchema = gql`
   type Query {
@@ -35,16 +42,22 @@ export const apiTypeDefs = mergeTypeDefs([
   userStatusSchema_API,
   userSettingsSchema,
   userProfileApollo,
+
   serverMemberSchema.API,
   serverSchema.API,
   serverEmoji.API,
+
   channel.API,
+  category.API,
+  channel_permission,
 ]);
 
 export const wsTypeDefs = mergeTypeDefs([
   linkedSchema,
   userStatusSchema_Ws,
-  serverSchema.Ws,
   userProfileWs,
+
+  serverSchema.Ws,
+
   channel.WS,
 ]);
