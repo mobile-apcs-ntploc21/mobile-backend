f8707c94ad5a357967b92e6e6696b9a83fbb46fd
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-09 22:40:21 +0700

    BE-69: Create model, typedefs, and resolver for Server Bans.

--- apollo/src/graphql/resolvers/index.ts
+++ apollo/src/graphql/resolvers/index.ts
@@ -1,14 +1,17 @@
-import { mergeResolvers } from '@graphql-tools/merge';
+import { mergeResolvers } from "@graphql-tools/merge";
 
-import userResolver from './user';
-import relationshipResolver from './relationship';
-import { userStatusResolvers_API, userStatusResolvers_Ws } from './user_status';
-import userSettingsResolvers from './userSettings';
-import { userProfileApollo, userProfileWs } from './user_profile';
-import serverResolver from './server';
-import serverEmoji from './serverEmoji';
-import { dummyResolver_Ws } from './dummy';
-import serverMemberResolver from './server_member';
+import { dummyResolver_Ws } from "./dummy";
+/// User
+import userResolver from "./user";
+import relationshipResolver from "./relationship";
+import { userStatusResolvers_API, userStatusResolvers_Ws } from "./user_status";
+import userSettingsResolvers from "./userSettings";
+import { userProfileApollo, userProfileWs } from "./user_profile";
+/// Server
+import serverResolver from "./server";
+import serverEmoji from "./serverEmoji";
+import serverMemberResolver from "./server_member";
+import serverBansResolver from "./server_bans";
 
 // Merge all resolvers: Add more in the future if needed
 // e.g [userResolver, postResolver, channelResolver]
@@ -21,11 +24,12 @@ export const apiResolvers = mergeResolvers([
   serverResolver.API,
   serverMemberResolver.API,
   serverEmoji.API,
+  serverBansResolver,
 ]);
 
 export const wsResolvers = mergeResolvers([
   dummyResolver_Ws,
   userStatusResolvers_Ws,
-  serverResolver.Ws,
   userProfileWs,
+  serverResolver.Ws,
 ]);
--- apollo/src/graphql/typedefs/index.ts
+++ apollo/src/graphql/typedefs/index.ts
@@ -1,17 +1,20 @@
-import { gql } from 'apollo-server-express';
-import { mergeTypeDefs } from '@graphql-tools/merge';
+import { gql } from "apollo-server-express";
+import { mergeTypeDefs } from "@graphql-tools/merge";
 
-import userSchema from './user';
-import relationshipSchema from './relationship';
-import { userStatusSchema_API, userStatusSchema_Ws } from './user_status';
-import serverSchema from './server';
-import serverMemberSchema from './server_member';
-import userSettingsSchema from './userSettings';
+/// User
+import userSchema from "./user";
+import relationshipSchema from "./relationship";
+import { userStatusSchema_API, userStatusSchema_Ws } from "./user_status";
+import userSettingsSchema from "./userSettings";
 import {
   apolloTypedefs as userProfileApollo,
   wsTypedefs as userProfileWs,
-} from './user_profile';
-import serverEmoji from './serverEmoji';
+} from "./user_profile";
+/// Server
+import serverSchema from "./server";
+import serverMemberSchema from "./server_member";
+import serverEmoji from "./serverEmoji";
+import serverBans from "./server_bans";
 
 const linkedSchema = gql`
   type Query {
@@ -37,11 +40,12 @@ export const apiTypeDefs = mergeTypeDefs([
   serverMemberSchema.API,
   serverSchema.API,
   serverEmoji.API,
+  serverBans.API,
 ]);
 
 export const wsTypeDefs = mergeTypeDefs([
   linkedSchema,
   userStatusSchema_Ws,
-  serverSchema.Ws,
   userProfileWs,
+  serverSchema.Ws,
 ]);
