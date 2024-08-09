d1833d392b34711c097ad56301a8f1941240a818
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-09 23:06:14 +0700

    BE-69: Handle transaction when create ban.

--- apollo/src/models/server_bans.ts
+++ apollo/src/models/server_bans.ts
@@ -1,24 +1,28 @@
 import mongoose, { model, Schema } from "mongoose";
 
 interface IServerBan {
-  server: { type: Schema.Types.ObjectId };
-  user: { type: Schema.Types.ObjectId };
+  _id: {
+    server: Schema.Types.ObjectId;
+    user: Schema.Types.ObjectId;
+  };
 }
 
 const serverBanSchema = new Schema<IServerBan>(
   {
-    server: {
-      type: Schema.Types.ObjectId,
-      ref: "Server",
-      required: [true, "Server ID is required!"],
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "User ID is required!"],
+    _id: {
+      server: {
+        type: Schema.Types.ObjectId,
+        ref: "Server",
+        required: [true, "Server ID is required!"],
+      },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User ID is required!"],
+      },
     },
   },
-  { timestamps: true, _id: false }
+  { timestamps: true }
 );
 
 const ServerBan = mongoose.model("ServerBan", serverBanSchema);
