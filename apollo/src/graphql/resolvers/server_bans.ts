9638fd4809b537a3b7d1e0a0a87af9a011f79e46
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-09 23:06:14 +0700

    BE-69: Handle transaction when create ban.

--- apollo/src/graphql/resolvers/server_bans.ts
+++ apollo/src/graphql/resolvers/server_bans.ts
@@ -1,29 +1,73 @@
 import { IResolvers } from "@graphql-tools/utils";
 import { AuthenticationError, UserInputError } from "apollo-server";
 import { GraphQLJSON } from "graphql-scalars";
+import mongoose from "mongoose";
 
+import ServerModel from "../../models/server";
 import ServerBansModel from "../../models/server_bans";
 import ServerMemberModel from "../../models/server_member";
+
+const CreateBanTransaction = async ({ server_id, user_id }) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    // Create server ban
+    const opts = { session, new: true };
+    const [createdBan] = await ServerBansModel.create(
+      [{ _id: { server: server_id, user: user_id } }],
+      opts
+    );
+
+    // Remove user from server members
+    const _ = await ServerMemberModel.findOneAndDelete(
+      { "_id.server": server_id, "_id.user": user_id },
+      opts
+    );
+
+    // Decrease server total members
+    const server = await ServerModel.findById(server_id).session(session);
+    server.totalMembers--;
+    await server.save();
+
+    await session.commitTransaction();
+    return createdBan;
+  } catch (error) {
+    await session.abortTransaction();
+    console.error(error);
+    throw new UserInputError("Cannot create server ban !");
+  } finally {
+    session.endSession();
+  }
+};
 
 const resolvers: IResolvers = {
   Query: {
     getServerBan: async (_, { server_id, user_id }) => {
       try {
         const serverBan = await ServerBansModel.findOne({
-          server: server_id,
-          user: user_id,
+          _id: { server: server_id, user: user_id },
         });
-        return serverBan;
+        return {
+          server: serverBan._id.server,
+          user: serverBan._id.user,
+        };
       } catch (error) {
         throw error;
       }
     },
     getServerBans: async (_, { server_id, limit }) => {
       try {
-        const serverBans = await ServerBansModel.find({ server: server_id })
+        const serverBans = await ServerBansModel.find({
+          "_id.server": server_id,
+        })
           .limit(limit)
           .sort({ createdAt: -1 });
-        return serverBans;
+
+        return serverBans.map((ban) => ({
+          server: ban._id.server,
+          user: ban._id.user,
+        }));
       } catch (error) {
         throw error;
       }
@@ -32,33 +76,37 @@ const resolvers: IResolvers = {
   Mutation: {
     createServerBan: async (_, { server_id, user_id }) => {
       try {
-        const serverBan = await ServerBansModel.create({
-          server: server_id,
-          user: user_id,
-        });
+        const serverBan = await CreateBanTransaction({ server_id, user_id });
 
-        // Remove user from server members
-        await ServerMemberModel.findOneAndDelete({
-          _id: { server_id, user_id },
-        });
-
-        return serverBan;
+        return {
+          server: serverBan._id.server,
+          user: serverBan._id.user,
+        };
       } catch (error) {
         throw error;
       }
     },
     createServerBulkBan: async (_, { server_id, user_ids }) => {
       try {
-        const serverBans = await ServerBansModel.insertMany(
-          user_ids.map((user_id) => ({ server: server_id, user: user_id }))
-        );
+        let success = [];
+        let failed = [];
 
-        // Remove users from server members
-        await ServerMemberModel.deleteMany({
-          _id: { $in: user_ids.map((user_id) => ({ server_id, user_id })) },
-        });
+        for (const user_id of user_ids) {
+          try {
+            const serverBan = await CreateBanTransaction({
+              server_id,
+              user_id,
+            });
+            success.push({
+              server: serverBan._id.server,
+              user: serverBan._id.user,
+            });
+          } catch (error) {
+            failed.push({ user: user_id, error: error.message });
+          }
+        }
 
-        return serverBans;
+        return success;
       } catch (error) {
         throw error;
       }
@@ -66,8 +114,7 @@ const resolvers: IResolvers = {
     deleteServerBan: async (_, { server_id, user_id }) => {
       try {
         const serverBan = await ServerBansModel.findOneAndDelete({
-          server: server_id,
-          user: user_id,
+          _id: { server: server_id, user: user_id },
         });
         return true;
       } catch (error) {
