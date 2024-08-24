6083f661a2743817c285762e46c7d1b4f206fb6b
Ngô Hoàng Tuan <rickielukas@gmail.com>
2024-08-24 11:11:10 +0700

    BE-72: Add channels basic mutations.

--- api/src/controllers/channel.ts
+++ api/src/controllers/channel.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import graphQLClient from "../utils/graphql";
 import { channelQueries } from "@/graphql/queries";
+import { channelMutations } from "@/graphql/mutations";
 
 const _getChannel = async (channel_id: string) => {
   try {
@@ -123,6 +124,154 @@ export const getChannelPermissions = async (
     }
 
     return res.status(200).json({ permissions });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const createChannel = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const server_id = req.params?.serverId;
+  const { name, category_id, is_private } = req.body;
+
+  if (!server_id || !name) {
+    return res.status(400).json({ message: "Server ID and name is required." });
+  }
+
+  try {
+    const channel = await graphQLClient().request(
+      channelMutations.CREATE_CHANNEL,
+      {
+        server_id,
+        input: {
+          name,
+          category_id,
+          is_private,
+        },
+      }
+    );
+
+    return res.status(200).json({ ...channel.createChannel });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const updateChannel = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const server_id = req.params?.serverId;
+  const channel_id = req.params?.channelId;
+  const { name, description, is_private, is_nsfw, is_archived, is_deleted } =
+    req.body;
+
+  if (!server_id) {
+    return res.status(400).json({ message: "Server ID is required." });
+  }
+
+  if (!channel_id) {
+    return res.status(400).json({ message: "Channel ID is required." });
+  }
+
+  try {
+    const channel = await graphQLClient().request(
+      channelMutations.UPDATE_CHANNEL,
+      {
+        channel_id,
+        input: {
+          name,
+          description,
+          is_private,
+          is_nsfw,
+          is_archived,
+          is_deleted,
+        },
+      }
+    );
+
+    return res.status(200).json({ ...channel.updateChannel });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deleteChannel = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const channel_id = req.params?.channelId;
+
+  if (!channel_id) {
+    return res.status(400).json({ message: "Channel ID is required." });
+  }
+
+  try {
+    await graphQLClient().request(channelMutations.DELETE_CHANNEL, {
+      channel_id,
+    });
+
+    return res.status(200).json({ message: "Channel deleted." });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const hardDeleteChannel = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const channel_id = req.params?.channelId;
+
+  if (!channel_id) {
+    return res.status(400).json({ message: "Channel ID is required." });
+  }
+
+  try {
+    await graphQLClient().request(channelMutations.HARD_DELETE_CHANNEL, {
+      channel_id,
+    });
+
+    return res.status(200).json({ message: "Channel hard deleted." });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const moveChannel = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const server_id = req.params?.serverId;
+  const channel_id = req.params?.channelId;
+  const { category_id, new_position } = req.body;
+
+  if (!server_id) {
+    return res.status(400).json({ message: "Server ID is required." });
+  }
+
+  if (!channel_id) {
+    return res.status(400).json({ message: "Channel ID is required." });
+  }
+
+  try {
+    const channel = await graphQLClient().request(
+      channelMutations.MOVE_CHANNEL,
+      {
+        channel_id,
+        category_id,
+        new_position,
+      }
+    );
+
+    return res.status(200).json({ ...channel.moveChannel });
   } catch (error) {
     return next(error);
   }
