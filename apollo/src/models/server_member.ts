52ec03fe154380c9330a145e83a7bdde077e850e
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-28 16:59:26 +0700

    BE-77: Update the server_member schema to have is_favorite and position.

--- apollo/src/models/server_member.ts
+++ apollo/src/models/server_member.ts
@@ -1,8 +1,10 @@
-import { model, Schema } from 'mongoose';
-import ModelNames from './modelNames';
+import { model, Schema } from "mongoose";
+import ModelNames from "./modelNames";
 
 interface IServerMember {
   _id: { server_id: Schema.Types.ObjectId; user_id: Schema.Types.ObjectId };
+  is_favorite?: boolean;
+  position?: number;
 }
 
 const schema = new Schema<IServerMember>(
@@ -11,13 +13,21 @@ const schema = new Schema<IServerMember>(
       server_id: {
         type: Schema.Types.ObjectId,
         ref: ModelNames.Server,
-        required: [true, 'Server ID is required!'],
+        required: [true, "Server ID is required!"],
       },
       user_id: {
         type: Schema.Types.ObjectId,
         ref: ModelNames.User,
-        required: [true, 'User ID is required!'],
+        required: [true, "User ID is required!"],
       },
+    },
+    is_favorite: {
+      type: Boolean,
+      default: false,
+    },
+    position: {
+      type: Number,
+      default: 0,
     },
   },
   { timestamps: true, _id: false }
