a21194e50cf5c65f9a0c3230dc800edbb618480a
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-08-30 21:03:30 +0700

    BE-80: Add apporiate permission to server and server emojis.

--- api/src/routes/servers/serverEmojis.ts
+++ api/src/routes/servers/serverEmojis.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction, Router } from "express";
-import { authMiddleware } from "../../utils/authMiddleware";
+import { checkServerPermissionMiddleware } from "../../utils/checkServerPermissionMiddleware";
+import { BaseRolePermissions as BRP } from "../../constants/permissions";
 
 import * as serverEmojiCtrl from "../../controllers/servers/serverEmojis";
 
@@ -9,14 +10,20 @@ const serverRouter = Router();
 serverRouter.get("/:serverId/emojis/:emojiId", serverEmojiCtrl.getServerEmoji);
 serverRouter.get("/:serverId/emojis/", serverEmojiCtrl.getServerEmojis);
 
-serverRouter.post("/:serverId/emojis/", serverEmojiCtrl.createServerEmoji);
+serverRouter.post(
+  "/:serverId/emojis/",
+  checkServerPermissionMiddleware([BRP.CREATE_EXPRESSION]),
+  serverEmojiCtrl.createServerEmoji
+);
 serverRouter.patch(
   "/:serverId/emojis/:emojiId",
+  checkServerPermissionMiddleware([BRP.MANAGE_EXPRESSION]),
   serverEmojiCtrl.updateServerEmoji
 );
 
 serverRouter.delete(
   "/:serverId/emojis/:emojiId",
+  checkServerPermissionMiddleware([BRP.MANAGE_EXPRESSION]),
   serverEmojiCtrl.deleteServerEmoji
 );
 
