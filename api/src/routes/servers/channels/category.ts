c2edc7632a50459d08fd67419d144ae491798a42
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-09-03 19:47:57 +0700

    BE-72: Rerouting and remove unnecessary import.

--- api/src/routes/servers/channels/category.ts
+++ api/src/routes/servers/channels/category.ts
@@ -1,16 +1,43 @@
 import { Router } from "express";
 import categoryPermissionRouter from "./category_permission";
+import { checkCategoryExistenceMiddleware } from "../../../utils/checkCategoryExistenceMiddleware";
+import { checkServerPermissionMiddleware } from "../../../utils/checkServerPermissionMiddleware";
+import {
+  BaseRolePermissions as BRP,
+  CategoryPermissions,
+  ChannelPermissions,
+} from "../../../constants/permissions";
 import * as categoryCtrl from "../../../controllers/servers/channels/category";
 
 const categoryRouter = Router({ mergeParams: true });
 
 // Category Role and User Permissions
-categoryRouter.use("/", categoryPermissionRouter);
+categoryRouter.use(
+  "/:categoryId",
+  checkCategoryExistenceMiddleware,
+  categoryPermissionRouter
+);
 
 // Category CRUD operations routes
-categoryRouter.post("/", categoryCtrl.createCategory);
-categoryRouter.patch("/:categoryId", categoryCtrl.updateCategory);
+categoryRouter.get("/", categoryCtrl.getCategories);
 
-categoryRouter.delete("/:categoryId", categoryCtrl.deleteCategory);
+categoryRouter.post("/", categoryCtrl.createCategory);
+categoryRouter.patch(
+  "/:categoryId",
+  checkCategoryExistenceMiddleware,
+  categoryCtrl.updateCategory
+);
+
+categoryRouter.delete(
+  "/:categoryId",
+  checkCategoryExistenceMiddleware,
+  categoryCtrl.deleteCategory
+);
+
+categoryRouter.patch(
+  "/:categoryId/move",
+  checkCategoryExistenceMiddleware,
+  categoryCtrl.moveCategory
+);
 
 export default categoryRouter;
