5fe9c077edb9fd2f76124741e676fdc6193efe2d
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-09-06 20:54:53 +0700

    BE-96: Remove the category/channel params.

--- api/src/utils/checkCategoryPermissionMiddleware.ts
+++ api/src/utils/checkCategoryPermissionMiddleware.ts
@@ -1,22 +1,28 @@
-import { Request, Response, NextFunction } from 'express';
-import {getUserCategoryPermissionsFunc} from "../utils/getUserCategoryPermissions";
+import { Request, Response, NextFunction } from "express";
+import { getUserCategoryPermissionsFunc } from "../utils/getUserCategoryPermissions";
 
-export const checkCategoryPermissionMiddleware = (requiredPermissions: string[]) => {
+export const checkCategoryPermissionMiddleware = (
+  requiredPermissions: string[]
+) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const { uid : user_id, server_id, category_id } = res.locals;
+    const { uid: user_id, server_id, category_id } = res.locals;
 
-    if (!user_id || !server_id || !category_id) {
-      return res.status(401).json({ message: 'Unauthorized' });
+    if (!user_id || !server_id) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const currentUserPermissions = await getUserCategoryPermissionsFunc(user_id, category_id, server_id);
+    const currentUserPermissions = await getUserCategoryPermissionsFunc(
+      user_id,
+      category_id,
+      server_id
+    );
 
     const hasPermission = requiredPermissions.every((permission) => {
-      return currentUserPermissions[permission] === 'ALLOWED';
+      return currentUserPermissions[permission] === "ALLOWED";
     });
 
     if (!hasPermission) {
-      return res.status(403).json({ message: 'Forbidden' });
+      return res.status(403).json({ message: "Forbidden" });
     }
     next();
   };
