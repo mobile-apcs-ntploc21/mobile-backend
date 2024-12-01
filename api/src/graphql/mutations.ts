d1c5149fb5786964963b792d1e5b03378606ea3e
iambluuu <dinhhoangviet19@gmail.com>
2024-12-01 20:51:42 +0700

    Reformat with Eslint

--- api/src/graphql/mutations.ts
+++ api/src/graphql/mutations.ts
@@ -1230,7 +1230,10 @@ export const cronjobMutations = {
 export const directMessageMutations = {
   CREATE_DIRECT_MESSAGE: gql`
     mutation createDirectMessage($user_first_id: ID!, $user_second_id: ID!) {
-      createDirectMessage(user_first_id: $user_first_id, user_second_id: $user_second_id) {
+      createDirectMessage(
+        user_first_id: $user_first_id
+        user_second_id: $user_second_id
+      ) {
         _id {
           user_first_id
           user_second_id
