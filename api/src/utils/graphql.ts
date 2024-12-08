5c4e8a21c785f0f4e9f13d5472e7e91176e660bb
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-12-08 14:10:27 +0700

    SE-53: Add cache for graphQLClient.

--- api/src/utils/graphql.ts
+++ api/src/utils/graphql.ts
@@ -1,10 +1,30 @@
 import { GraphQLClient } from "graphql-request";
 import config from "../config";
 
-export default function graphQLClient(token?: string) {
+type RequestCache =
+  | "default"
+  | "no-store"
+  | "reload"
+  | "no-cache"
+  | "force-cache"
+  | "only-if-cached";
+
+type Options = {
+  cache?: RequestCache;
+};
+
+const graphQLClient = (
+  token?: string,
+  options: Options = {}
+): GraphQLClient => {
+  const { cache = "default" } = options;
+
   return new GraphQLClient(`${config.GQL_SERVER_URL}/graphql`, {
     headers: {
       ...(token && { authorization: `Bearer ${token}` }),
     },
+    cache: cache,
   });
-}
+};
+
+export default graphQLClient;
