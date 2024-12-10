e8c1444ea86304e0a5ebe6e840c052f1adc30f4a
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-12-10 23:33:21 +0700

    SE-55: Implement orders model typedefs and resolvers.

---
+++ apollo/src/graphql/typedefs/payment/orders.ts
@@ -0,0 +1,44 @@
+import { gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Order {
+    id: ID!
+    user_id: ID!
+    package_id: ID!
+
+    amount: Int!
+    status: String!
+    transaction_id: String!
+
+    createdAt: String!
+  }
+`;
+
+const gqlAPI = gql`
+  extend type Query {
+    orders: [Order]
+    order(id: ID!): Order
+    ordersByUser(user_id: ID!): [Order]
+  }
+
+  extend type Mutation {
+    createOrder(
+      user_id: ID!
+      package_id: ID!
+      amount: Int!
+      status: String!
+      transaction_id: String!
+    ): Order!
+    updateOrder(
+      id: ID!
+      amount: Int
+      status: String
+      transaction_id: String
+    ): Order!
+    deleteOrder(id: ID!): Boolean!
+  }
+`;
+
+const API = [typeDefs, gqlAPI];
+
+export default { API };
