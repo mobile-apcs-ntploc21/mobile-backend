333775d668014997a903d5d73d1814fd088b2224
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2024-12-15 20:56:08 +0700

    SE-55: Support subscriptions resolvers and creation.

--- apollo/src/constants/features_value.ts
+++ apollo/src/constants/features_value.ts
@@ -5,14 +5,17 @@ interface IFeaturesValue {
 const Features = {
   CROSS_SERVER_EMOJIS: "bCSE",
   ATTACHMENT_SIZE_LIMIT: "nASL",
+  CONTENT_MESSAGE_LIMIT: "nCML",
 };
 
-const FreePackageFeatures: IFeaturesValue = {
+export const FreePackageFeatures: IFeaturesValue = {
   [Features.CROSS_SERVER_EMOJIS]: true,
   [Features.ATTACHMENT_SIZE_LIMIT]: 10,
+  [Features.CONTENT_MESSAGE_LIMIT]: 2000,
 };
 
-const PremiumPackageFeatures: IFeaturesValue = {
+export const PremiumPackageFeatures: IFeaturesValue = {
   [Features.CROSS_SERVER_EMOJIS]: true,
   [Features.ATTACHMENT_SIZE_LIMIT]: 50,
+  [Features.CONTENT_MESSAGE_LIMIT]: 4000,
 };
