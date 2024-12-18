fde62d72e0a150d6644d2a1701db8f9f34b926ca
iambluuu <dinhhoangviet19@gmail.com>
2024-12-18 10:04:12 +0700

    SE-41-API: Change getDirectMessage api to match messageCtrl

--- api/src/controllers/servers/message.ts
+++ api/src/controllers/servers/message.ts
@@ -541,15 +541,10 @@ export const deleteMessage = async (
   }
 
   if (message.sender_id !== res.locals.uid) {
-    // Get the user role in the channel
-    const permissions = res.locals.userChannelPermissions;
-
-    if (!permissions || permissions?.MANAGE_MESSAGE !== "ALLOWED") {
-      res.status(403).json({
-        message: "You do not have permission to delete this message.",
-      });
-      return;
-    }
+    res.status(403).json({
+      message: "You do not have permission to delete this message.",
+    });
+    return;
   }
 
   try {
