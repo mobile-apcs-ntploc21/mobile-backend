fd568d2ccfdcfdbc8be3e9ee792769e11a57e619
iambluuu <dinhhoangviet19@gmail.com>
2024-12-18 10:04:12 +0700

    SE-41-API: Change getDirectMessage api to match messageCtrl

--- api/src/controllers/direct_message.ts
+++ api/src/controllers/direct_message.ts
@@ -34,20 +34,66 @@ export const getDirectMessage = async (
 ) => {
   const { conversationId } = req.params;
 
-  if (!conversationId) {
-    res.status(400).json({ error: "Conversation ID is required!" });
+  const { limit } = req.query;
+  const { before, after, around } = req.query;
+
+  if (!user_first_id || !user_second_id) {
+    res.status(400).json({ error: "User IDs are required!" });
     return;
   }
 
+  if (limit && isNaN(parseInt(limit as string))) {
+    res.status(400).json({ message: "Limit must be a number." });
+    return;
+  }
+
+  if (user_first_id > user_second_id) {
+    const temp = user_first_id;
+    user_first_id = user_second_id;
+    user_second_id = temp;
+  }
+
   try {
-    const directMessage = await _getDirectMessage(conversationId).catch(
-      () => null
-    );
+    let directMessage = await _getDirectMessage(
+      user_first_id,
+      user_second_id
+    ).catch(() => null);
+
     if (!directMessage) {
-      res.status(404).json({ error: "Direct message not found!" });
+      directMessage = await _createDirectMessage(
+        user_first_id,
+        user_second_id
+      ).catch(() => null);
+
+      if (!directMessage) {
+        res.status(500).json({
+          message: "Conversation does not exist, failed to get direct message.",
+        });
+        return;
+      }
+    }
+
+    const requestBody = {
+      conversation_id: directMessage.conversation_id,
+      limit: parseInt(limit as string) || 50,
+      before: before,
+      after: after,
+      around: around,
+    };
+
+    const { messages } = await graphQLClient().request(
+      messageQueries.GET_MESSAGES,
+      requestBody
+    );
+
+    if (!messages) {
+      // Return empty array if no messages found
+      res.status(200).json({ messages: [] });
       return;
     }
-    res.status(200).json(directMessage);
+
+    res.status(200).json({ messages });
+    return;
   } catch (error) {
     next(error);
   }
@@ -75,7 +121,100 @@ export const createDirectMessage = async (
   const { user_first_id, user_second_id } = req.body;
 
   if (!user_first_id || !user_second_id) {
-    res.status(400).json({ error: "User IDs are required!" });
+    res.status(400).json({ message: "User IDs are required." });
+    return;
+  }
+
+  if (user_first_id === user_second_id) {
+    res.status(400).json({ message: "User IDs must be different." });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).json({ message: "Content is required." });
+    return;
+  }
+
+  if (!req.query) {
+    res.status(400).json({ message: "Query is required." });
+    return;
+  }
+
+  if (user_first_id > user_second_id) {
+    const temp = user_first_id;
+    user_first_id = user_second_id;
+    user_second_id = temp;
+  }
+
+  const author_id = _author_id
+    ? Array.isArray(_author_id)
+      ? _author_id
+      : [_author_id]
+    : [];
+  const mentions = _mentions
+    ? Array.isArray(_mentions)
+      ? _mentions
+      : [_mentions]
+    : [];
+  const has = _has ? (Array.isArray(_has) ? _has : [_has]) : [];
+
+  try {
+    const requestBody = {
+      query: {
+        text: content,
+        from: author_id,
+        mention: mentions,
+        has: has,
+      },
+      offset: (parseInt(page as string) - 1) * 25 || 0,
+      limit: parseInt(limit as string) || 25,
+    };
+
+    const { searchMessages: messages } = await graphQLClient().request(
+      messageQueries.SEARCH_MESSAGES,
+      requestBody
+    );
+
+    if (!messages) {
+      // Return empty array if no messages found
+      res.status(200).json({ messages: [] });
+      return;
+    }
+
+    res.status(200).json({ messages });
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const getPinnedMessages = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  let user_first_id = res.locals.uid;
+  let user_second_id = req.params?.userId as string;
+
+  if (!user_first_id || !user_second_id) {
+    res.status(400).json({ message: "User IDs are required." });
+    return;
+  }
+
+  if (user_first_id === user_second_id) {
+    res.status(400).json({ message: "User IDs must be different." });
+    return;
+  }
+
+  if (user_first_id > user_second_id) {
+    const temp = user_first_id;
+    user_first_id = user_second_id;
+    user_second_id = temp;
+  }
+
+  const conversation = await _getDirectMessage(user_first_id, user_second_id);
+
+  if (!conversation) {
+    res.status(404).json({ message: "Conversation not found." });
     return;
   }
 
--- api/src/routes/direct_message.ts
+++ api/src/routes/direct_message.ts
@@ -6,172 +6,23 @@ import {
   deleteDirectMessage,
 } from "../controllers/direct_message";
 
-/**
- * @swagger
- * tags:
- *  name: DirectMessage
- *
- * components:
- *  schemas:
- *    UserProfile:
- *      type: object
- *      properties:
- *        user_first_id:
- *          type: string
- *        user_second_id:
- *         type: string
- *        conversation_id:
- *         type: string
- *        lastest_message_id:
- *         type: string
- *        has_new_message:
- *         type: boolean
- *        number_of_unread_mentions:
- *         type: integer
- */
-
 const directMessageRouter = Router();
 
-/**
- * @swagger
- *  /directMessages/{conversationId}:
- *    get:
- *      summary: Retrieve a specific direct message by conversation ID.
- *      description: Retrieve a specific direct message by its conversation ID.
- *      tags: [Direct Messages]
- *      parameters:
- *        - name: conversationId
- *          in: path
- *          required: true
- *          description: The ID of the conversation to retrieve.
- *          schema:
- *            type: string
- *      responses:
- *        '200':
- *          description: Successful response with the direct message information.
- *          content:
- *            application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $ref: '#/components/schemas/DirectMessage'
- *        '400':
- *          description: Missing conversation ID.
- *          content:
- *            application/json:
- *              example:
- *                message: Conversation ID is required.
- *        '404':
- *          description: Direct message not found.
- *          content:
- *           application/json:
- *              example:
- *                  message: Direct message not found.
- *
- */
-directMessageRouter.get("/:conversationId", getDirectMessage);
+// Get all DMs
+directMessageRouter.get("/me", getDirectMessages);
+// Get messages from DM with a user
+directMessageRouter.get("/:userId", getDirectMessage);
 
-/**
- * @swagger
- *  /directMessages/user/{userId}:
- *    get:
- *      summary: Retrieve all direct messages by a user ID.
- *      description: Retrieve all direct messages by a user ID.
- *      tags: [Direct Messages]
- *      parameters:
- *        - name: userId
- *          in: path
- *          required: true
- *          description: The ID of the user.
- *          schema:
- *            type: string
- *      responses:
- *        '200':
- *          description: Successful response with the direct messages.
- *          content:
- *            application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $ref: '#/components/schemas/DirectMessage'
- *        '400':
- *          description: Missing user ID.
- *          content:
- *            application/json:
- *              example:
- *                message: User ID is required.
- *        '401':
- *          $ref: '#/components/responses/AuthMiddlewareError'
- */
-directMessageRouter.get("/user/:userId", getDirectMessages);
+directMessageRouter.put("/:messageId", messageCtrl.editMessage);
+directMessageRouter.delete("/:messageId", messageCtrl.deleteMessage);
+directMessageRouter.post("/:userId", createMessage);
+directMessageRouter.post("/:userId/upload", uploadFile);
+directMessageRouter.get("/:userId/pins", getPinnedMessages);
+directMessageRouter.post("/:messageId/pin", messageCtrl.pinMessage);
+directMessageRouter.delete("/:messageId/pin", messageCtrl.unpinMessage);
 
-/**
- * @swagger
- *  /directMessages:
- *    post:
- *      summary:
- *      tags: [Direct Messages]
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              properties:
- *                refreshToken:
- *                  type: string
- *      responses:
- *        201:
- *          description: Successful login
- *          content:
- *            application/json:
- *              schema:
- *                type: object
- *                properties:
- *                  user_first_id:
- *                    type: string
- *                  user_second_id:
- *                    type: string
- *                  conversation_id:
- *                    type: string
- *        400:
- *          description: Missing required fields
- *          content:
- *            application/json:
- *              example:
- *                message: Missing User ID(s)
- */
-directMessageRouter.post("/", createDirectMessage);
-
-/**
- * @swagger
- * /directMessages/{conversationId}:
- *  delete:
- *    summary: Delete a direct message by conversation ID.
- *    tags: [Direct Messages]
- *    parameters:
- *      - name: conversationId
- *        in: path
- *        required: true
- *        description: The ID of the conversation to delete.
- *        schema:
- *          type: string
- *    responses:
- *      204:
- *        description: Successful response with the direct message information.
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/DirectMessage'
- *      400:
- *        description: Missing conversation ID.
- *        content:
- *          application/json:
- *            example:
- *              message: Conversation ID is required.
- */
-directMessageRouter.delete("/:conversationId", deleteDirectMessage);
+directMessageRouter.get("/:messageId/reactions", messageCtrl.getReactions);
+directMessageRouter.post("/:messageId/reactions", messageCtrl.reactMessage);
+directMessageRouter.delete("/:messageId/reactions", messageCtrl.unreactMessage);
 
 export default directMessageRouter;
