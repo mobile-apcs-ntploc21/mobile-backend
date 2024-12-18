cc2760c8b2a09bee91f9e8e1ebf7d818890c3f1a
iambluuu <dinhhoangviet19@gmail.com>
2024-12-18 17:44:23 +0700

    SE-51: Add cleanup emoji logic, hotfix ServerEmojiModel to EmojiModel in emoji.ts resolver

--- api/src/graphql/queries.ts
+++ api/src/graphql/queries.ts
@@ -825,11 +825,22 @@ export const messageQueries = {
       }
     }
   `,
+};
 
+export const cronjobQueries = {
   GET_AVAILABLE_ATTACHMENTS: gql`
     query availableAttachments {
       availableAttachments {
         url
+      }
+    }
+  `,
+  GET_DELETED_EMOJIS: gql`
+    query deletedEmojis {
+      deletedEmojis {
+        id
+        name
+        image_url
       }
     }
   `,
