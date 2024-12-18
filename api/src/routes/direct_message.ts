9c68eca32ed7db8c3a7214b1c56a61249492bc8d
iambluuu <dinhhoangviet19@gmail.com>
2024-12-18 10:34:03 +0700

    SE-41-API: Add read api, rewrite deleteMessage api

--- api/src/routes/direct_message.ts
+++ api/src/routes/direct_message.ts
@@ -5,6 +5,9 @@ import {
   uploadFile,
   getPinnedMessages,
   createMessage,
+  deleteMessage,
+  editMessage,
+  readMessages,
 } from "../controllers/direct_message";
 import * as messageCtrl from "../controllers/servers/message";
 
@@ -15,10 +18,13 @@ directMessageRouter.get("/me", getDirectMessages);
 // Get messages from DM with a user
 directMessageRouter.get("/:userId", getDirectMessage);
 
-directMessageRouter.put("/:messageId", messageCtrl.editMessage);
-directMessageRouter.delete("/:messageId", messageCtrl.deleteMessage);
+directMessageRouter.put("/:messageId", editMessage);
+directMessageRouter.delete("/:messageId", deleteMessage);
+
 directMessageRouter.post("/:userId", createMessage);
 directMessageRouter.post("/:userId/upload", uploadFile);
+
+directMessageRouter.post("/:userId/read", readMessages);
 directMessageRouter.get("/:userId/pins", getPinnedMessages);
 directMessageRouter.post("/:messageId/pin", messageCtrl.pinMessage);
 directMessageRouter.delete("/:messageId/pin", messageCtrl.unpinMessage);
